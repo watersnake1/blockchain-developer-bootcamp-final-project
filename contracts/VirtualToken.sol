35555672c87c17a52faaef8edfe1421e347451b5
Christian Murray <christian@Christians-MacBook-Pro.local>
2021-11-21 13:00:15 -0500

    fix bugs based on the results of test

--- contracts/VirtualToken.sol
+++ contracts/VirtualToken.sol
@@ -1,8 +1,13 @@
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 // step one: create a wireframe of the functions that will be needed
 
 contract VirtualToken is ERC20 {
+	using SafeMath for uint;
 	// data structures
 	// mirroring the structure of ERC20 from OZ where the balance map is private
 	mapping (address => uint256) private _stakedBalances;
@@ -11,30 +16,21 @@ contract VirtualToken is ERC20 {
 	uint stakingPenalty;
 	uint transferReward;
 	uint totalStaked;
+	uint eventCount;
 	
 	// who owns the contract
 	address public owner;
 
-	// refresh on this syntax at somepoint
-	// need to also call the ERC20 constructor here
-	constructor() {
-		a = 1;
-		stakingPenalty = 0.5;
-		totalStaked = 0;
-		transferReward = 0.001;
-		owner = msg.sender;
-	}
-
 	// needed modifiers: 
 	//	sufficientUnstakedBalance
 	//	sufficientStakedBalance
-	modifier sufficientUnstakedBalance(address _act, _amt) {
-		require(balanceOf[_act] > _amt, "Insufficient Unstaked Balance");
+	modifier sufficientUnstakedBalance(address _act, uint _amt) {
+		require(balanceOf(_act) > _amt, "Insufficient Unstaked Balance");
 		_;
 	}
 
-	modifier sufficientStakedBalance(address _act, _amt) {
-		require(stakedBalanceOf[_act] > _amt, "Insufficient Staked Balance");
+	modifier sufficientStakedBalance(address _act, uint _amt) {
+		require(stakedBalanceOf(_act) > _amt, "Insufficient Staked Balance");
 		_;
 	}
 
@@ -44,9 +40,30 @@ contract VirtualToken is ERC20 {
 	event UnstakeEvent(uint256 amt, address unstaker, uint newA);
 	//	TransferEvent
 
-	event TransferEvent(uint256 newA);
+	event NewMultiplierEvent(uint256 newA);
 
 	// functions
+	// refresh on this syntax at somepoint
+	// need to also call the ERC20 constructor here
+	constructor(string memory name_,
+		   string memory symbol_)
+	ERC20(name_, symbol_) {
+		//require(owner_ != address(0), "ZERO_OWNER");
+		owner = msg.sender;
+		//a = 1.01;
+		a = 1010000000000000000;
+		//stakingPenalty = 0.5;
+		stakingPenalty = 500000000000000000;
+		totalStaked = 0;
+		//transferReward = 0.001;
+		transferReward = 1000000000000000; 
+		eventCount = 0;
+		//proxyRegistryAddress = proxy_;
+		//uint256 totalSupply = 100000000000000000000;
+		uint256 totalSupply = 100;
+		_mint(msg.sender,totalSupply); 
+	}
+	//todo: finish checking decimal notation, implement unstake and stake rewards/penalties, then we're done!	
 
 	/**
 	* Returns the balance of the user that is in staking mapping
@@ -59,7 +76,8 @@ contract VirtualToken is ERC20 {
 	* Returns the virtual staked balance that is in the staking mapping, or above x a
 	*/
 	function stakedVirtualBalanceOf(address act) public view returns (uint256) {
-		return _stakedBalances[act] * a;
+		//return _stakedBalances[act] * a;
+		return _stakedBalances[act].mul(a);
 	}
 
 	/**
@@ -68,38 +86,60 @@ contract VirtualToken is ERC20 {
 	*/
 	function modifyMultiplier(uint _delta, bool _increase) private returns (bool) {
 		// code to change the multiplier value
+		if (_increase) {
+			eventCount = eventCount.add(1);
+		} else {
+			if (eventCount > 0) {	
+				eventCount = eventCount.sub(1);	
+			} else {
+				return false;
+			}
+		}
+		a = a**eventCount;
+		return true;
 	}
 
 	/**
 	* Move tokens from the _balances mapping to the staked balance mapping
 	*/
-	function stake(uint _amt) sufficientUnstakedBalance(msg.sender, _amt) returns (bool) {
-		_balances[msg.sender] -= _amt;
-		_stakedBalances[msg.sender] += _amt;
+	function stake(uint _amt) public sufficientUnstakedBalance(msg.sender, _amt) returns (bool) {
+		//_balances[msg.sender] -= _amt;
+		//_balances[msg.sender].sub(_amt);
+		_burn(msg.sender, _amt);
+		//_stakedBalances[msg.sender] += _amt;
+		_stakedBalances[msg.sender] = _stakedBalances[msg.sender].add(_amt);
 		// decrease the multiplier
 		modifyMultiplier(stakingPenalty, false);
-		totalStaked += _amt;
+		//totalStaked += _amt;
+		totalStaked = totalStaked.add(_amt);
+		// increase the event count to increase a
+		emit StakeEvent(_amt, msg.sender, getMultiplier());
 		return true;
 	}
 
 	/**
 	* Move tokens from the staked balance mapping to the regular _balances mapping
 	*/
-	function unstake(uint _amt) sufficientStakedBalance(msg.sender, _amt) returns (bool) {
-		_balances[msg.sender] += _amt;
-		_stakedBalances[msg.sender] -= _amt;
+	function unstake(uint _amt) public sufficientStakedBalance(msg.sender, _amt) returns (bool) {
+		//_balances[msg.sender] += _amt;
+		//_balances[msg.sender].add(_amt);
+		//_stakedBalances[msg.sender] -= _amt;
+		_stakedBalances[msg.sender] = _stakedBalances[msg.sender].sub(_amt);
 		modifyMultiplier(transferReward, true);
-		totalStaked -= _amt;
+		//totalStaked -= _amt;
+		totalStaked = totalStaked.sub(_amt);
+		_mint(msg.sender, _amt);
 		return true;	
 	}
 
 	/**
 	* Need to override transfer to add the custom multiplier increase behavior
 	*/
-	function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
+	function transfer(address recipient, uint256 amount) public override returns (bool) {
 		//increase the multiplier
 		modifyMultiplier(transferReward, true);
 		_transfer(_msgSender(), recipient, amount);
+		emit NewMultiplierEvent(getMultiplier());
 		return true;
 	}
 
@@ -114,16 +154,24 @@ contract VirtualToken is ERC20 {
 	* Get the total staked balance within the protocol in virtual terms
 	*/
 	function getTotalVirtualStakeBalance() public view returns (uint) {
-		return totalStaked * a;
+		//return totalStaked * a;
+		return totalStaked.mul(a);
 	}	
 
 	/**
 	* Get the multiplier a
 	*/
 	function getMultiplier() public view returns (uint) {
-		return a;
+		return eventCount;
 	}
 	// to consider:
 	// add ownability, and allow for the changing of parameters by the admin
+
+	/**
+	* Get the current owner of the contract
+        */
+        function getOwner() public view returns (address) {
+		return owner;
+	}
 }
 
