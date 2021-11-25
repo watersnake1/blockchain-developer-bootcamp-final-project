9cebaf260fd52e5eee99e3586b69213c113575b7
Christian Murray <christian@Christians-MacBook-Pro.local>
2021-11-25 11:53:12 -0500

    add comments showing attack vectors prevented; clean up code

--- contracts/VirtualToken.sol
+++ contracts/VirtualToken.sol
@@ -4,9 +4,12 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-// step one: create a wireframe of the functions that will be needed
 
+// @title: Virtual Token - Final Project
+// @author: Christian Murray
 contract VirtualToken is ERC20 {
+	// SafeMath prevents attackers from manipulating integer overflows in math operations to exploit the contract
+	// (attack vector one)
 	using SafeMath for uint;
 	// data structures
 	// mirroring the structure of ERC20 from OZ where the balance map is private
@@ -21,9 +24,7 @@ contract VirtualToken is ERC20 {
 	// who owns the contract
 	address public owner;
 
-	// needed modifiers: 
-	//	sufficientUnstakedBalance
-	//	sufficientStakedBalance
+	// using these modifiers prevents minting tokens that a user does not have (attack vector 2)
 	modifier sufficientUnstakedBalance(address _act, uint _amt) {
 		require(balanceOf(_act) > _amt, "Insufficient Unstaked Balance");
 		_;
@@ -38,13 +39,10 @@ contract VirtualToken is ERC20 {
 	//	UnstakeEvent
 	
 	event UnstakeEvent(uint256 amt, address unstaker, uint newA);
-	//	TransferEvent
 
 	event NewMultiplierEvent(uint256 newA);
 
 	// functions
-	// refresh on this syntax at somepoint
-	// need to also call the ERC20 constructor here
 	constructor(string memory name_,
 		   string memory symbol_)
 	ERC20(name_, symbol_) {
@@ -63,7 +61,6 @@ contract VirtualToken is ERC20 {
 		uint256 totalSupply = 100;
 		_mint(msg.sender,totalSupply); 
 	}
-	//todo: finish checking decimal notation, implement unstake and stake rewards/penalties, then we're done!	
 
 	/**
 	* Returns the balance of the user that is in staking mapping
@@ -84,7 +81,7 @@ contract VirtualToken is ERC20 {
 	* Makes a change to the global multiplier
         * If _increase == true, then it is a positive change, if false then it is a decrease
 	*/
-	function modifyMultiplier(uint _delta, bool _increase) private returns (bool) {
+	function modifyMultiplier(bool _increase) private returns (bool) {
 		// code to change the multiplier value
 		if (_increase) {
 			eventCount = eventCount.add(1);
@@ -109,7 +106,7 @@ contract VirtualToken is ERC20 {
 		//_stakedBalances[msg.sender] += _amt;
 		_stakedBalances[msg.sender] = _stakedBalances[msg.sender].add(_amt);
 		// decrease the multiplier
-		modifyMultiplier(stakingPenalty, false);
+		modifyMultiplier(false);
 		//totalStaked += _amt;
 		totalStaked = totalStaked.add(_amt);
 		// increase the event count to increase a
@@ -119,16 +116,13 @@ contract VirtualToken is ERC20 {
 
 	/**
 	* Move tokens from the staked balance mapping to the regular _balances mapping
+        * Accrue the virtual yield gains to the user's wallet
 	*/
 	function unstake(uint _amt) public sufficientStakedBalance(msg.sender, _amt) returns (bool) {
-		//_balances[msg.sender] += _amt;
-		//_balances[msg.sender].add(_amt);
-		//_stakedBalances[msg.sender] -= _amt;
 		_stakedBalances[msg.sender] = _stakedBalances[msg.sender].sub(_amt);
-		modifyMultiplier(transferReward, true);
-		//totalStaked -= _amt;
+		modifyMultiplier(true);
 		totalStaked = totalStaked.sub(_amt);
-		_mint(msg.sender, _amt);
+		_mint(msg.sender, _amt.mul(getMultiplier()));
 		return true;	
 	}
 
@@ -137,7 +131,7 @@ contract VirtualToken is ERC20 {
 	*/
 	function transfer(address recipient, uint256 amount) public override returns (bool) {
 		//increase the multiplier
-		modifyMultiplier(transferReward, true);
+		modifyMultiplier(true);
 		_transfer(_msgSender(), recipient, amount);
 		emit NewMultiplierEvent(getMultiplier());
 		return true;
@@ -154,7 +148,6 @@ contract VirtualToken is ERC20 {
 	* Get the total staked balance within the protocol in virtual terms
 	*/
 	function getTotalVirtualStakeBalance() public view returns (uint) {
-		//return totalStaked * a;
 		return totalStaked.mul(a);
 	}	
 
